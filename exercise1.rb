edeba61107a8441d2daf602c643248cc68c6d928
naderae <naderabouezze93@gmail.com>
2017-06-26 15:10:54 -0400

    2nd

--- exercise1.rb
+++ exercise1.rb
@@ -2,4 +2,12 @@ puts 2
 puts 3
 puts 2 + 3
 
-puts 2 puts 3 puts 2 + 3
+puts 2
+puts 3
+  puts 2 + 3
+puts "Hello\t\tworld"
+puts "\a"
+
+puts "Hello\b\b\b\b\bGoodbye World"
+puts "Hello\rStart over World"
+puts "1. Hello \n2.World"
---
+++ exercise2.rb
@@ -0,0 +1,7 @@
+puts 55 *(1.15)
+puts 55.to_s + "Dogs"
+
+puts "55.to_s"
+puts "thirty".to_i
+puts "there are #{45628 * 7839} dogs in my house"
+puts (10 < 20 && 30 < 20) || !(10 == 11)
